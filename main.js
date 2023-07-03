1d1870fb5f992b2a2bb642b6a8aac54972d3fca0
IrynaVelykholova <irynavely93@gmail.com>
2023-07-03 09:45:00 +0200

    modificato <= 18 anni

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const anni = prompt('Quanti anni hai?')
 const km = prompt('Quanti km devi percorrere?');
 const costo = parseInt(km) * 0.21;
 
-    if (anni < 18) {
+    if (anni <= 18) {
         document.getElementById("prezzo").innerHTML = (costo - (costo * 20) / 100 ).toFixed(2);
     } else if (anni >= 65) {
         document.getElementById("prezzo").innerHTML = (costo - (costo * 20) / 100 ).toFixed(2);
